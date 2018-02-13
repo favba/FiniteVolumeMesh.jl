6528161285a786c02ab6bcc24eb9fda175947cd3
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-13 11:36:44 -0500

    Geneeric gradient and div

--- src/derivatives.jl
+++ src/derivatives.jl
@@ -1,4 +1,4 @@
-function gradient!(out::Array{<:AbstractVec,1}, f_at_face, f_at_bface, bf2c,bfn,bcv,f2c,fn,cv,floop::Face2CellLoop{NBF,NF,VecType}) where {NBF,NF,VecType}
+function gradient!(out::Vector, f_at_face, f_at_bface, bf2c,bfn,bcv,f2c,fn,cv,floop::Face2CellLoop{NBF,NF,VecType}) where {NBF,NF,VecType}
   fill!(out,zero(eltype(out)))
   @inbounds for i=1:NBF
     j = bf2c[i][1]
@@ -20,7 +20,7 @@ end
 
 @inline gradient!(out, ff, fbf, floop) = gradient!(out, ff, fbf, floop.bf2c,floop.bfn,floop.bcv,floop.f2c,floop.fn,floop.cv,floop)
 
-function div!(out::Array{<:Number,1}, f_at_face, f_at_bface, bf2c, bfn, bcv, f2c, fn, cv, floops::Face2CellLoop{NBF,NF,VecT}) where {NBF,NF,VecT}
+function div!(out::Vector, f_at_face, f_at_bface, bf2c, bfn, bcv, f2c, fn, cv, floops::Face2CellLoop{NBF,NF,VecT}) where {NBF,NF,VecT}
   fill!(out,zero(eltype(out)))
   @inbounds for i=1:NBF
     el = bf2c[i]
