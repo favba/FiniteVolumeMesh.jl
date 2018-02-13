558c36c1a57f14f79e85b6cd93a7f8b5df9fca88
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-12 20:49:24 -0500

    Some exports, include utils and redefine abstract types

--- src/FiniteVolumeMesh.jl
+++ src/FiniteVolumeMesh.jl
@@ -1,16 +1,20 @@
 __precompile__()
 module FiniteVolumeMesh
 
+export HomogeneousMesh, Vec, Vec2D, TriangleCell
+
+include("utils.jl")
+
 abstract type AbstractVec{T} <: DenseArray{T,1} end
 
-abstract type AbstractCell end
-abstract type Abstract2DCell <: AbstractCell end
+abstract type AbstractCell2Node{NN,NF} <: NTupleWrap{NN,UInt} end
+abstract type AbstractCell2Node2D{NN,NF} <: AbstractCell2Node{NN,NF} end
 
-abstract type AbstractFace end
-abstract type AbstractBoundaryFace end
+abstract type AbstractFace2Node{N} <: NTupleWrap{N,UInt} end
+const AbstractFace2Node1D = AbstractFace2Node{1}
+const AbstractFace2Node2D = AbstractFace2Node{2}
 
 abstract type AbstractMesh end
-abstract type Abstract2DMesh <: AbstractMesh end
 
 include("vec.jl")
 include("geometry.jl")
