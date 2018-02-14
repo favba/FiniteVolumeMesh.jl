18c1ef7956e05ebdc5a219930e5524c50cc0b1b6
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-14 15:35:47 -0500

    Iteration methods for NTupleWrap

--- src/utils.jl
+++ src/utils.jl
@@ -5,6 +5,10 @@ Base.eltype(a::NTupleWrap{N,T}) where {N,T} = T
 Base.length(a::NTupleWrap{N,T}) where {N,T} = N
 Base.size(t::NTupleWrap, d) = (d == 1) ? length(t) : throw(ArgumentError("invalid tuple dimension $d"))
 
+Base.start(t::NTupleWrap) = start(t.ind)
+Base.next(t::NTupleWrap,i) = next(t.ind,i)
+Base.done(t::NTupleWrap,i) = done(t.ind,i)
+
 Base.:(==)(a::NTupleWrap,b) = a.ind == b
 Base.:(==)(b,a::NTupleWrap) = a.ind == b
 Base.:(==)(a::NTupleWrap,b::NTupleWrap) = a.ind == b.ind
