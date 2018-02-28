abb858734fba09f3fdd50a3753add7bf0bda50ba
Felipe A. V. de Bragança Alves <felipe.avba@gmail.com>
2018-02-28 16:01:04 -0500

    FieldAtBoundary struct to calculate on-the-fly the value at boundary.

--- src/boundaryconditions.jl
+++ src/boundaryconditions.jl
@@ -13,6 +13,26 @@ function flux_dirichlet(a::Dirichlet,bfc,bfn,k,bccenter,Tc)
   return qf
 end
 
+struct FieldAtBoundary{T,BcondType,VecType}
+  Tc::Array{T}
+  bf2c::Array{BFace2Cell}
+  bt::Array{UInt}
+  bcond::BcondType
+  bfc::Array{VecType}
+  bccenter::Array{VecType}
+  bfn::Array{VecType}
+end
+
+function FieldAtBoundary(Tc,mesh,bcond)
+  FieldAtBoundary{eltype(Tc),typeof(bcond),eltype(mesh.nodes)}(Tc,mesh.f2cloops.bf2c,mesh.f2cloops.bft,bcond,mesh.f2cloops.bfc, mesh.f2cloops.bccenter,mesh.f2cloops.bfn)
+end
+
+@inline Base.@propagate_inbounds function Base.getindex(fab::FieldAtBoundary,i)
+  j = fab.bf2c[i][1]
+  a = fab.bcond[fab.bt[i]] 
+  return typeof(a) <: Dirichlet ? val(a) : fab.Tc[j] + ((fab.bfc[i] - fab.bccenter[i])⋅fab.bfn[i])*flux(a)/norm(fab.bfn[i])
+end
+
 function boundary_conditions(d::Dict)
   btypes = d[:BCs]
   bcond = ()
