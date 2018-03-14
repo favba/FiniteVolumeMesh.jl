28cb5866af740a2b8223c788c0ca545e2c809a41
Felipe Alves <felipe.avba@gmail.com>
2018-03-14 15:40:51 -0400

    Added fullupwind advection.

--- src/advection.jl
+++ src/advection.jl
@@ -31,3 +31,35 @@ function (a::UpWind2ndOrder)(rhs,p)
   floops = p.mesh.f2cloops
   a(rhs, p.ρC, p.u, p.u, p.Tc, p.Tbf, p.∇Tc, floops.bf2c, floops.bfn, floops.bcv, floops.f2c, floops.fn, floops.cv, floops.fc, floops.ccenter, floops)
 end
+
+struct FullUpWind <: AbstractAdvection
+end
+
+function (a::FullUpWind)(rhs, ρC, buvec, uvec, Tc, Tbf, bf2c, bfn, bcv, f2c, fn, cv, fc, ccenter, floops::Face2CellLoop)
+  NBF = nbfaces(floops)
+  @inbounds for i=1:NBF
+    j = bf2c[i][1]
+    rhs[j] -= (ρC * Tbf[i] * (buvec[i] ⋅ bfn[i]))*bcv[i]
+  end
+  NF = nfaces(floops)
+  @inbounds for i=1:NF
+    j1, j2 = f2c[i]
+    n = fn[i]
+    v1, v2 = cv[i]
+    udotn = uvec[i] ⋅ n
+    if udotn >= 0
+      Tf = Tc[j1]
+    else
+      Tf = Tc[j2]
+    end
+    qf = ρC*Tf*udotn
+    rhs[j1] -= qf*v1
+    rhs[j2] += qf*v2 
+  end
+
+end
+
+function (a::FullUpWind)(rhs,p)
+  floops = p.mesh.f2cloops
+  a(rhs, p.ρC, p.u, p.u, p.Tc, p.Tbf, floops.bf2c, floops.bfn, floops.bcv, floops.f2c, floops.fn, floops.cv, floops.fc, floops.ccenter, floops)
+end
